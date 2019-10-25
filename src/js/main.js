35d823dd392c8a2a1077cbf5b140a24f4e270596
Ciarmin <Fator001@gmail.com>
2019-10-25 22:02:48 +0200

    feat: upload repo

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { parse } from "path";
+
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
@@ -19,4 +21,33 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+const counter = document.querySelector('.hydro__image--counter--js');
+const add = document.querySelector('.hydro__button--add--js');
+const remove = document.querySelector('.hydro__button--remove--js');
+const key = new Date().toISOString().slice(0, 10)
 
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0)
+  counter.innerHTML = '0';
+} else {
+  counter.innerHTML = localStorage.getItem(key);
+}
+
+
+
+
+add.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  counter.innerHTML = localStorage.getItem(key) ;
+
+})
+
+remove.addEventListener('click', (e) => {
+  const currentConter = parseInt(localStorage.getItem(key));
+  if (currentConter > 0) {
+    localStorage.setItem(key, localStorage.getItem(key) - 1);
+    counter.innerHTML = currentConter -1;
+  }
+
+})
